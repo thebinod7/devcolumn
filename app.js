72e7f24c630c13fed48df1c8ff0dddedae49064b
Binod <thebinod7@gmail.com>
2020-06-13 10:20:38 +0545

    basic setup

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const helmet = require("helmet");
 
 const RouteManager = require("./routes");
 const config = require("config");
 
-//mongoose.Promise = global.Promise;
-mongoose.connect(config.get("app.db"));
-
+// Connect to database using mongoose.
+mongoose.connect(config.get("app.db_url"));
 mongoose.connection.on("connected", function () {
   console.log("Connected to database successfully.");
 });
@@ -22,47 +20,27 @@ mongoose.connection.on("error", function (err) {
 
 const app = express();
 
+// To log requests inside app.
 app.use(morgan("combined"));
-app.use(helmet());
 
-const port = Number(process.env.PORT || 8888);
+const port = Number(config.get("app.port") || 8888);
 
-// view engine setup
+// view engine setup to ejs.
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// To parse data from form body.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type,authorization"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-// ROUTES FOR OUR API
+// ROUTES FOR OUR APP
 app.use("/", RouteManager);
+
+// To serve static files inside public folder. eg: js, css, images.
 app.use(express.static(path.join(__dirname, "public")));
 
-//Start server
+//Start server at given port number.
 app.listen(port, function () {
   console.log("Server running at port:" + port);
 });
