ee72a6bac94d262aa28f5fc19c78e3bbde6bf7b5
Binod <thebinod7@gmail.com>
2020-06-27 12:36:54 +0545

    add play file

---
+++ play/array_methods.js
@@ -0,0 +1,13 @@
+let fruits = "apple,banana,mango";
+let _array = fruits.split(","); // Output: ['apple', 'banana','mango']
+
+let input_array = ["apple", "banana", "mango"];
+let res = input_array.toString(); // Output: apple, banana, mango
+
+let today = new Date();
+let new_date = new Date();
+new_date.setDate(today.getDate() + 2);
+
+console.log("Date after adding 2 days:", new_date);
+
+//Output: Date after adding 2 days: 2020-06-28T06:50:14.104Z
