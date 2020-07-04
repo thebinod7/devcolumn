3a6790f9249d5e495ea9f0d34c50f44150665254
Binod <thebinod7@gmail.com>
2020-07-04 15:47:03 +0545

    2-csrf-token-xs-request-forgery-secure

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const csrf = require("csurf");
+
+const csrfProtection = csrf({ cookie: true });
 
 router.get("/", (req, res, next) => {
   const data = {
@@ -6,6 +9,16 @@ router.get("/", (req, res, next) => {
     message: "Welcome to million dollar app ;)",
   };
   res.render("index", data);
+});
+
+router.get("/form", csrfProtection, function (req, res) {
+  console.log("REQ:", req.csrfToken());
+  // pass the csrfToken to the view
+  res.render("submitForm", { csrfToken: req.csrfToken() });
+});
+
+router.post("/process", csrfProtection, function (req, res) {
+  res.send("CSRF Token passed.");
 });
 
 module.exports = router;
